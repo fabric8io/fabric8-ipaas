4ae71f3dc6ba3ea696221f159edc529bc1aabe23
Charles Moulliard <cmoulliard@apache.org>
2016-05-26 10:43:34 +0200

    Include a ssl.sh script as we have with apiman

---
+++ apiman-gateway/ssl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# creating the cert
+mvn clean compile -Pssl
+# deleting existing secret
+oc delete secret apiman-gateway-keystore
+# setting the secret
+oc secrets new apiman-gateway-keystore keystore=target/secret/apiman-gateway/keystore
+# oc label secret/apiman-gateway-keystore apiman-infra # make them easier to delete later
+# mvn -Pf8-local-deploy -Pssl
+# oc delete route apiman-gateway
+# oc create route passthrough apiman-gateway --service apiman-gateway
