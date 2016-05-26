d2b4c85581c15b9b2ae58b158706e7f8f5069410
Charles Moulliard <cmoulliard@apache.org>
2016-05-26 12:09:50 +0200

    Add missing key password file

--- apiman-gateway/ssl.sh
+++ apiman-gateway/ssl.sh
@@ -5,7 +5,7 @@ mvn clean compile -Pssl
 # deleting existing secret
 oc delete secret apiman-gateway-keystore
 # setting the secret
-oc secrets new apiman-gateway-keystore keystore=target/secret/apiman-gateway/keystore
+oc secrets new apiman-gateway-keystore keystore=target/secret/apiman-gateway/keystore keystore-password=target/secret/apiman-gateway/keystore.password
 # oc label secret/apiman-gateway-keystore apiman-infra # make them easier to delete later
 # mvn -Pf8-local-deploy -Pssl
 # oc delete route apiman-gateway
